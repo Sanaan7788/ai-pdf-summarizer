1dad1c0031144e6899c6ecf0d6a0d093cce2f174
Sanaan7788 <sanaan7788@gmail.com>
2025-01-10 13:31:51 -0600

    backend done

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import streamlit as st #this library is used for creating UI
 import os #for environment variable management
+from utils import *
 
 def main():
     st.set_page_config(page_title="PDF summarizer")
@@ -11,6 +12,16 @@ def main():
     pdf=st.file_uploader("Upload your pdf document",type="pdf")
 
     submit =st.button("Generate Summary")
+
+    os.environ["OPEN_AI_KEY"]="XXXXXXXXXXXXX"
+    # os.environ["OPEN_AI_KEY"] = os.getenv("XXXXXXXXXXXXXXXXXXXXXX")
+
+
+    if submit:
+        response=summarizer(pdf)
+        st.subheader("Summary of File:")
+        st.write(response)
+
 
 if __name__=="__main__":
     main()
